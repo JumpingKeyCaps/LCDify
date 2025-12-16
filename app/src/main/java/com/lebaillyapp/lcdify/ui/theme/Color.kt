4a3c11ee6f013b2c724e94a4c1bea355e075abbb
Samuel Lebailly <sam.meusa@gmail.com>
2025-12-16 20:31:29 +0100

    feature(ui/theme): Add App Color palette to match the nintendo vibe.

--- app/src/main/java/com/lebaillyapp/lcdify/ui/theme/Color.kt
+++ app/src/main/java/com/lebaillyapp/lcdify/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.lebaillyapp.lcdify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Game Boy DMG-01 shell palette
+val GB_ShellPrimary   = Color(0xFFC4C1B8) // coque principale
+val GB_ShellSecondary = Color(0xFFB2B0A6) // relief / surfaces
+val GB_ShellDivider   = Color(0xFF9E9C94) // separators subtils
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Controls
+val GB_ButtonAB   = Color(0xFF7B2C4B) // A / B
+val GB_DPad       = Color(0xFF3A3A3A) // croix + start/select
+
+// Text
+val GB_LabelText  = Color(0xFF2E4A62) // sérigraphie Game Boy
+val GB_TextDark   = Color(0xFF1E1E1E)
+val GB_TextLight  = Color(0xFFF5F5F5)
