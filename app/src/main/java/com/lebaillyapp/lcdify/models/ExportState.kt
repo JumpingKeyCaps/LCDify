6358d9a9c0fcf8dd229b208a5ffae0277d56d3f0
Samuel Lebailly <sam.meusa@gmail.com>
2026-01-05 14:33:25 +0100

    chore(architecture/): Remove and clean old architecture wireframe for a pure MVVM.

--- app/src/main/java/com/lebaillyapp/lcdify/models/ExportState.kt
+++
@@ -1,8 +0,0 @@
-package com.lebaillyapp.lcdify.models
-
-sealed class ExportState {
-    object Idle : ExportState()
-    data class Processing(val progress: Float) : ExportState() // 0.0 to 1.0
-    data class Success(val filePath: String) : ExportState()
-    data class Error(val message: String) : ExportState()
-}
